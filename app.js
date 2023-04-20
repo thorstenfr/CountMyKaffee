a7bc99d65e7c4dc5d8730b16510527c8039c99f2
thorstenfr <tfreimann@gmail.com>
2023-04-20 07:52:03 +0200

    Einführung localstorage

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const clickButton = document.getElementById("click-button");
 const clickCount = document.getElementById("click-count");
-let count = 0;
+
+let count = localStorage.getItem("count") || 0; // get count from localStorage, default to 0
+
+clickCount.innerHTML = count; // set initial count display
 
 clickButton.addEventListener("click", () => {
   count++;
   clickCount.innerHTML = count;
+  localStorage.setItem("count", count); // save count to localStorage
 });
